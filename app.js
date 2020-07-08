da67605dab4aa879b6bdbd4adfe890524ece6c85
SarahRector <rector.sarah.r@gmail.com>
2020-07-08 15:52:54 -0700

    added basic layout and getRandomNumber function

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // import functions and grab DOM elements
+import getRandomThrow from './randomThrow.js';
+
+const button = document.getElementById('guess-button');
 
 // initialize state
+let randomNumber = 1;
 
 // set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    console.log(getRandomThrow(randomNumber));
+});
+
+
+//compare randomThrow to userThrow to see if user wins or not
+
+//log guesses, wins, losses and update textContent
---
+++ randomThrow.js
@@ -0,0 +1,11 @@
+export default function getRandomThrow() {
+
+    const randomNumber = Math.ceil(Math.random() * 3);
+    
+    if (randomNumber === 1) {
+        return 'rock';       
+    } else if (randomNumber === 2) {
+        return 'paper';
+    } else return 'scissors';
+}
+    
